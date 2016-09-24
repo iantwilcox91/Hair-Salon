ed34b385626a3222f17b69899036cef2e7271c76
ian Wilcox <ian Wilcox>
2016-09-23 18:18:02 -0700

    add forms

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -9,7 +9,23 @@ namespace HairSalon
     public Homemodule()
     {
       Get["/"] = _ => {
-        return View["index.cshtml"];
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["index.cshtml", AllStylists];
+      };
+
+      Get["/stylists"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
+
+      Get["/addAnotherStylist"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["addAnotherStylist.cshtml", AllClients];
+      };
+
+      Get["/addNewClientToStylist"] = _ => {
+        List<Client> AllClients = Client.GetAll();
+        return View["addNewClientToStylist.cshtml", AllClients];
       };
     }
   }
